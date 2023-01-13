adf447aaccf4900db030000a1a5856759d858059
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2023-01-13 17:57:48 +0100

    Add initial configuration + design settings

---
+++ recipe-manager-app/src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import NewRecipe from "./components/NewRecipe/NewRecipe";
+
+function App() {
+  return (
+    <Fragment>
+      <NewRecipe />
+    </Fragment>
+  );
+}
+
+export default App;
